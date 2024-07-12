e3e95670ef918c83ec7e9c74848ef77c8ace5728
Ollysheep <toune.rorpg@gmail.com>
2024-07-12 12:20:01 +0200

    V.4 Test : Unit test for character-card

--- src/app/components/character-card/character-card.component.spec.ts
+++ src/app/components/character-card/character-card.component.spec.ts
@@ -1,19 +1,17 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
+import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CharacterCardComponent } from './character-card.component';
+import { Character } from '../../models/character';
 
 describe('CharacterCardComponent', () => {
   let component: CharacterCardComponent;
   let fixture: ComponentFixture<CharacterCardComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ CharacterCardComponent ]
-    })
-    .compileComponents();
+      declarations: [CharacterCardComponent],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -24,5 +22,31 @@ describe('CharacterCardComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display character details', () => {
+    const character: Character = {
+      id: 1,
+      name: 'Rick Sanchez',
+      status: 'Alive',
+      species: 'Human',
+      type: '',
+      gender: 'Male',
+      origin: { name: 'Earth (C-137)', url: '' },
+      location: { name: 'Earth (Replacement Dimension)', url: '' },
+      image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
+      episode: [],
+      url: '',
+      created: '',
+    };
+    component.character = character;
+    fixture.detectChanges();
+
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('h2').textContent).toContain('Rick Sanchez');
+    expect(compiled.querySelector('img').src).toContain(character.image);
+    expect(compiled.querySelector('.details').textContent).toContain(
+      'Status: Alive | Species: Human | Character ID: 1'
+    );
   });
 });
