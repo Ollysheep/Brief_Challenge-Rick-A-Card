b664bd90387087db8011f2fc0636e6a0041b4850
Ollysheep <toune.rorpg@gmail.com>
2024-07-12 12:49:21 +0200

    V.7 Add : modules in app.module.ts - Modify : draw-button html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DrawButtonComponent } from './components/draw-button/draw-button.component';
+import { CharacterCardComponent } from './components/character-card/character-card.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, DrawButtonComponent, CharacterCardComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
